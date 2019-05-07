b0c4bc409b1918f636b1f8d168bedf9c80844b38
yaros72 <yaroslav.zhumagulov@gmail.com>
2019-05-07 15:34:20 +0300

    linear response

--- TDSE.py
+++ TDSE.py
@@ -1,6 +1,5 @@
 import numba
 import numpy as np
-import xc
 from tqdm import tqdm
 from scipy import linalg
 from ase.units import Hartree, Bohr
@@ -24,6 +23,7 @@ class TDSE(object):
             self.fqn[q]=kpt.f_n
             self.E[q]=np.diag(kpt.eps_n)
         self.get_momentum_matrix()
+        
     def get_momentum_matrix(self):
         self.momentum=np.zeros((3,self.NK,self.nbands,self.nbands),dtype=np.complex)
         for q in range(self.NK):
@@ -37,20 +37,51 @@ class TDSE(object):
                         
     def current(self):
         return np.einsum('qn,qin,qjn,dqij->d',self.fqn,self.wfn.conj(),self.wfn,self.momentum)
-        
-    def propagate(self,dt,steps,A):
-        self.wfn=np.zeros((self.NK,self.nbands,self.nbands),dtype=np.complex)
-        for q in range(self.NK):
-            self.wfn[q]=np.eye(self.nbands)        
+    
+    def linear_response(self,dt,steps,A0=[0,0,1e-5]):
+        self.A0=A0
         self.J=np.zeros((steps,3),dtype=np.complex)
+        
         I=np.eye(self.nbands)
+        H=self.E+np.einsum('iqnm,i->qnm',self.momentum,A0)+I*np.linalg.norm(A0)**2
+        self.wfn=np.zeros((self.NK,self.nbands,self.nbands),dtype=np.complex)
+        
+        for q in range(self.NK):
+            E,D=np.linalg.eigh(H[q])
+            self.wfn[q]=D
+            
         for t in tqdm(range(steps)):
             self.J[t]=self.current()
-            H=self.E+np.einsum('iqnm,i->qnm',self.momentum,A(t*dt))
             for q in range(self.NK):
-                H_left = I+0.5j*dt*H[q]            
-                H_right= I-0.5j*dt*H[q]
-                self.wfn[q]=linalg.solve(H_left, H_right@self.wfn[q]) 
+                H_left = I+0.5j*dt*self.E[q]            
+                H_right= I-0.5j*dt*self.E[q]
+                self.wfn[q]=linalg.solve(H_left, np.dot(H_right,self.wfn[q]))
+        J=self.J[:,2]
+        time=np.arange(J.size)*dt
+        freq = np.fft.fftfreq(J.size, d=dt)
+        freq=np.sort(freq);freq=freq[np.abs(freq)<10]
+        sigma=np.zeros(freq.size,dtype=complex)
+        for w in range(freq.size):
+            sigma[w]=np.trapz(J*np.exp(1j*freq[w]*time),time)/time.size
+        sigma=-sigma/A0[2]
+        epsilon=1+4*np.pi*1j*sigma/freq
+        return epsilon,freq
+        
+        
+        
+#     def propagate(self,dt,steps,A):
+#         self.wfn=np.zeros((self.NK,self.nbands,self.nbands),dtype=np.complex)
+#         for q in range(self.NK):
+#             self.wfn[q]=np.eye(self.nbands)        
+#         self.J=np.zeros((steps,3),dtype=np.complex)
+#         I=np.eye(self.nbands)
+#         for t in tqdm(range(steps)):
+#             self.J[t]=self.current()
+#             H=self.E+np.einsum('iqnm,i->qnm',self.momentum,A(t*dt))+I*np.linalg.norm(A(t*dt))**2
+#             for q in range(self.NK):
+#                 H_left = I+0.5j*dt*H[q]            
+#                 H_right= I-0.5j*dt*H[q]
+#                 self.wfn[q]=linalg.solve(H_left, np.dot(H_right,self.wfn[q]))
                 
                 
                 
